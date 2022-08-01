8c7b383ac8d24906689bbc07b2144326dde196ab
thisisofficial <thisisofficiallyofficial@gmail.com>
2022-07-31 21:39:40 -0500

    Reformatted Data, Exporting and Filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,96 @@
-import { useState, useEffect } from "react";
-import {
-  DataGrid,
-  GridColDef,
-  GridRowsProp,
-  GridToolbar,
-} from "@mui/x-data-grid";
-import styled from "@mui/styled-engine-sc";
-import "./App.css";
+import  { useState, useEffect } from 'react' 
+import {DataGrid, GridColDef, GridRowsProp, GridToolbar, GridValueFormatterParams} from '@mui/x-data-grid'
+import styled from '@mui/styled-engine-sc'
+import './App.css'
+
 
 function App() {
+  
+  function numberWithCommas(n: number){
+    return n.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+  }
   const [arr, setArr] = useState([]);
+  
+  const columns: GridColDef[] =[ 
+    { field: 'date', headerName: "ID",  width: 150},
+    { field: 'lastModified', headerName: "Date",  width: 150, type: 'date', valueFormatter: (params: GridValueFormatterParams<string>) =>{
+      if (params.value == null){
+        return '';
+      }
+      const valueFormatted = new Date(params.value).toString();
+      const [week, month, day, year, time, extra1, extra2] = valueFormatted.split(' ');
+      const valueReformatted =  week + ' ' + month + ' ' + day + ' ' + year
+      return valueReformatted;
+    },},
+    { field: 'states', headerName: "Infected States",  width: 150, type: 'number'},
 
-  const columns: GridColDef[] = [
-    { field: "date", headerName: "ID", width: 150 },
-    { field: "states", headerName: "Infected States", width: 150 },
-    { field: "positive", headerName: "Positive", width: 150 },
-    { field: "negative", headerName: "Negative", width: 150 },
-    { field: "pending", headerName: "Pending", width: 150 },
-    { field: "death", headerName: "Deaths", width: 150 },
-    { field: "totalTestResults", headerName: "Total", width: 150 },
-    { field: "hospitalized", headerName: "Hospitalized", width: 150 },
-    { field: "deathIncrease", headerName: "Death Increase", width: 150 },
-    {
-      field: "hospitalizedIncrease",
-      headerName: "Hospitalized Increase",
-      width: 150,
-    },
-  ];
+    //Positive
+    { field: 'positive', headerName: "Positive",  width: 150, type: 'number' , valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},
+    { field: 'positiveIncrease', headerName: "Positive Increase",  width: 150, type: 'number', valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},
+
+    //Negative 
+    { field: 'negative', headerName: "Negative",  width: 150, type: 'number', valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},
+    { field: 'negativeIncrease', headerName: "Negative Increase",  width: 150, type: 'number', valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},
+
+    //Pending
+    { field: 'pending', headerName: "Pending",  width: 150, type: 'number', valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},
+
+    //Deaths
+    { field: 'death', headerName: "Deaths",  width: 150, type: 'number', valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},
+    { field: 'deathIncrease', headerName: "Death Increase",  width: 150, type: 'number', valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},
+
+    //Total 
+    { field: 'totalTestResults', headerName: "Total Tests",  width: 150, type: 'number', valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},
+    { field: 'totalTestResultsIncrease', headerName: "Total Increase",  width: 150, type: 'number', valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},
+
+    //Hospitilized
+    { field: 'hospitalized', headerName: "Hospitalized",  width: 150, type: 'number', valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},
+    { field: 'hospitalizedIncrease', headerName: "Hospitalized Increase",  width: 170, type: 'number', valueFormatter:(params: GridValueFormatterParams<number>) =>{
+      if (params.value == null){
+        return '0';
+      }
+      return numberWithCommas(params.value)},},];
+  
 
   const getData = async () => {
     const getter = await fetch(
@@ -34,37 +98,45 @@ function App() {
     );
     const data = await getter.json();
     setArr(data);
-  };
+  }
+
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [])
 
-  const rows: GridRowsProp = arr;
+  const rows: GridRowsProp =  arr;
+
+  
 
   return (
     <div className="App">
       <div className="table-container">
-        <DataGrid
+      <DataGrid
         className="datagrid-table"
-          getRowId={(row) => row.date}
-          rows={rows}
-          columns={columns}
-          initialState={{
-            columns: {
-              columnVisibilityModel: {
-                pending: false,
-                date: false,
-                deathIncrease: false,
-                hospitalizedIncrease: false,
-              },
+        getRowId={(row) => row.date}
+        rows={rows}
+        columns={columns}
+        rowsPerPageOptions={[10, 25, 50, 100]}
+        initialState={{
+          columns: {
+            columnVisibilityModel: {
+              date: false,
+              deathIncrease: false,
+              positiveIncrease: false,
+              negativeIncrease: false,
+              hospitalizedIncrease: false,
             },
-          }}
+          },
+          pagination: {
+            pageSize: 25,
+          },
+         }}
           components={{ Toolbar: GridToolbar }}
-        />
-      </div>
+      />
+    </div>
     </div>
   );
 }
 
-export default App;
+export default App
