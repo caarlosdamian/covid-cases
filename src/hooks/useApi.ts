2a9b9536287ab858759e59ac70759db65f0e1d08
DiegoVillasenorVilla <divivi.619@gmail.com>
2022-08-03 14:14:17 -0500

    Add data to Local Storage

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
+import useLocalStorage from "./useLocalStorage"
 
 export const useApi = () => {
-  const [fetchData, setFetchData] = useState([]);
+  const [fetchData, setFetchData] = useLocalStorage('COVID_CASES_DATA', []);
 
   const getData = async () => {
     const getter = await fetch(
