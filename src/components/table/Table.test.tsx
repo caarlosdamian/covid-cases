010ade1ee1f77a09d3a0cc1e10ede32737c49e86
thisisofficial <thisisofficiallyofficial@gmail.com>
2022-08-09 16:16:50 -0500

    Added Jest Tests to Table component
    Co-authored-by: Carlos Damian <dapecarlos@gmail.com>

---
+++ src/components/table/Table.test.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable */
+
+import { fireEvent, render, screen } from '@testing-library/react';
+import { columns } from '../../utils/tableColumns';
+import { mockRows } from '../../utils/MockData';
+import { Table } from './Table';
+
+describe('Components Render Correctly', () => {
+  beforeAll(() => {
+    render(<Table rows={mockRows} columns={columns} />);
+  });
+  it('Renders Table without crashing', () => {
+    console.log(screen.debug());
+  });
+  // Date should be shown while ID should not
+  it('Renders columns Correctly', () => {
+    expect(screen.queryByText('Date')).toBeDefined();
+    expect(screen.queryByText('ID')).toBeNull();
+  });
+  // ID can be turned on and off by the Columns
+  it.skip('Shows hidden Columns', async () => {
+    fireEvent.click(screen.getByText('Columns'));
+    fireEvent.click(screen.getByText('ID'));
+    fireEvent.click(screen.getByText('Columns'));
+    expect(await screen.findAllByText('ID')).toBeDefined();
+  });
+});
