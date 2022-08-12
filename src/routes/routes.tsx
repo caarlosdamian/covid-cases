4a0762be7cf50343ae42f1a826dd25c8f7e904df
thisisofficial <thisisofficiallyofficial@gmail.com>
2022-08-12 11:37:43 -0500

    Addressed comments raised by CD

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,26 +3,23 @@ import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from '../pages';
 import { DataGrid } from '../components/DataGrid';
-import { loginContext } from '../hooks/loginContext';
 import { ProtectedRoute } from './protectedRoute';
 
 export function Router() {
-  const [userLog, setUserLog] = useState(false);
+  const userLog = useState(false);
   return (
-    <loginContext.Provider value={{ userLog, setUserLog }}>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute user={userLog} redirectPath="/login">
-                <DataGrid />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </loginContext.Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute user={userLog} redirectPath="/login">
+              <DataGrid />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
