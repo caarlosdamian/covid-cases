585bce8534a7a470cde89ae1122cad7655fc3342
DiegoVillasenorVilla <divivi.619@gmail.com>
2022-08-31 14:51:29 -0500

    fix some error in the commit

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,14 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
 import loginSliceReducer from './loginSlice';
 import toggleSliceReducer from './toggleSlice';
 import { useApi } from '../hooks/useApi';
 
-export const store = configureStore({
-  reducer: {
-    [useApi.reducerPath]: useApi.reducer,
-    loginSlice: loginSliceReducer,
-    toggleSlice: toggleSliceReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(useApi.middleware),
+const reducers = combineReducers({
+  [useApi.reducerPath]: useApi.reducer,
+  loginSlice: loginSliceReducer,
+  toggleSlice: toggleSliceReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false,
+});
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: customizedMiddleware,
+});
+
+export const persistor = persistStore(store);
