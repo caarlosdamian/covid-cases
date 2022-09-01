486e2eedc9f545c56336b45aadac695ccb8e6ad0
Diego Villaseñor Villa <50335824+DiegoVillasenorVilla@users.noreply.github.com>
2022-09-01 17:49:43 -0500

    FP-19 Redux persist (#31)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/Store';
 import App from './App';
-import { store } from './redux/Store';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,14 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import loginSliceReducer from './loginSlice';
 import toggleSliceReducer from './toggleSlice';
 import { useApi } from '../hooks/useApi';
 
-export const store = configureStore({
-  reducer: {
-    [useApi.reducerPath]: useApi.reducer,
-    loginSlice: loginSliceReducer,
-    toggleSlice: toggleSliceReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(useApi.middleware),
+const reducers = combineReducers({
+  [useApi.reducerPath]: useApi.reducer,
+  loginSlice: loginSliceReducer,
+  toggleSlice: toggleSliceReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false,
+});
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: customizedMiddleware,
+});
+
+export const persistor = persistStore(store);
