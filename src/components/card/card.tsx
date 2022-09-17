ff40f39e8a413f459db1e5bfffd7ba1998dea83a
DiegoVillasenorVilla <divivi.619@gmail.com>
2022-09-17 12:37:00 -0500

    Responsive desing added and some thinks that I fixed

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -5,17 +5,6 @@ export interface ICard {
   value: number;
 }
 
-/* export const CardComponent = ({ label, value }: ICard) => (
-  <div className="card-container">
-    <div className="card-item" data-testid="card-testing">
-      <div className="card-content-info">
-        <span className="card-label">{label}</span>
-        <p className="card-value">{value}</p>
-      </div>
-    </div>
-  </div>
-); */
-
 export const CardComponent = ({ label, value }: ICard) => (
   <div className="card-container" data-testid="card-testing">
     <span className="card-label">{label}</span>
