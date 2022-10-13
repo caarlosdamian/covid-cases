f5b34e510ff91c338b6f4d9cbfbcc2a2a1ac6ea2
thisisofficial <thisisofficiallyofficial@gmail.com>
2022-10-13 12:15:56 -0500

    More Fixes Comments by CD

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -99,25 +99,5 @@ export interface State {
 }
 
 export interface IChartProps {
-  dataSet: {
-    date: string;
-    confirmed: number;
-    deaths: number;
-    recovered: number;
-    confirmed_diff: number;
-    deaths_diff: number;
-    recovered_diff: number;
-    last_update: string;
-    active: number;
-    active_diff: number;
-    fatality_rate: number;
-    region: {
-      iso: string;
-      name: string;
-      province: string;
-      lat: string;
-      long: string;
-      cities: never[];
-    };
-  }[];
+  dataSet: covidMexData[];
 }
