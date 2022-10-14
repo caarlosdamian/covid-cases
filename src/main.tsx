09c777346b0225a6dc9ccc6ff7a4206797ded00b
Erik Damian <thisisofficiallyofficial@gmail.com>
2022-10-14 07:37:31 -0500

    [FP-30]-Add translations (#38)

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,36 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
 import { store, persistor } from './redux/Store';
 import App from './App';
 import './index.scss';
+import common_es from './translations/es/common.json';
+import common_en from './translations/en/common.json';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'es',
+  resources: {
+    en: {
+      common: common_en,
+    },
+    es: {
+      common: common_es,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <I18nextProvider i18n={i18next}>
+          <App />
+        </I18nextProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
